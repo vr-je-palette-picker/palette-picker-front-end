9dd5fe2117f5fc96932c78c9fd4e4cf55bacde5a
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-07 15:39:14 -0700

    Make App a class component and add random hex code generator method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      colors: [],
+      projects: []
+    };
+  }
+
+  randomColor = () => {
+    return Math.floor(Math.random() * 16777215).toString(16);
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Palette Picker</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
