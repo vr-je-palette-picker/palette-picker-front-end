c1e67ba112fe804d5f0409cd99ae746205cdde8d
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-12-08 09:02:25 -0700

    Add background color

--- src/Components/ColorCard/ColorCard.js
+++ src/Components/ColorCard/ColorCard.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
+import './ColorCard.scss'
 
 class ColorCard extends Component {
   constructor({ hex }) {
     super();
     this.state = {
       locked: false,
-      hex,
+      hex: hex
     }
   }
 
   render() {
     return (
     <div>
-      <h4>{ this.state.hex }</h4>
+      <h4 style={{backgroundColor: this.state.hex}}>{ this.state.hex }</h4>
     </div>
     )
   }
 }
-
+// style={{backgroundColor: bgColors.Yellow}}
 export default ColorCard;
