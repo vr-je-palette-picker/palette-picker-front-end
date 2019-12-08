16c36b8de617418c16ae93e77796677f8fafe289
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-12-08 09:08:32 -0700

    Refactor to put colorCards in container

---
+++ src/Components/Container/Container.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ColorCard from './../ColorCard/ColorCard.js'
+
+const Container = ({ hexCodes }) => {
+  const colorCards = hexCodes.map(hex => {
+    return <ColorCard hex={hex}/>
+  })
+  return (
+    <main>
+      <section>
+          { colorCards }
+        </section>
+    </main>
+  )
+}
+
+export default Container;
