5b8babc8ebe068bae66ad2f69151549fff1e2256
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-12-08 09:08:32 -0700

    Refactor to put colorCards in container

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ColorCard from './../ColorCard/ColorCard.js';
+import Container from './../Container/Container.js';
 import './App.scss';
 
 class App extends Component {
@@ -26,16 +26,10 @@ class App extends Component {
   };
 
   render() {
-    const colorCards = this.state.hexCodes.map(hex => {
-      return <ColorCard hex={hex}/>
-    })
-    console.log('colorCards', colorCards)
     return (
       <div className='App'>
         <h1>Palette Picker</h1>
-        <section>
-          { colorCards }
-        </section>
+        <Container hexCodes = {this.state.hexCodes}/>
       </div>
     );
   }
