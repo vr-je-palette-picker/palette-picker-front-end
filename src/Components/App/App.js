8b161275fa960fdf473c42aa7df2692210b354d6
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-08 20:38:08 -0700

    Build out Nav and HomeNav components, add icons for refresh, save, and all projects

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Nav from '../Nav/Nav.js';
 import Container from './../Container/Container.js';
 import ProjectContainer from './../ProjectContainer/ProjectContainer.js';
 import './App.scss';
@@ -11,67 +12,66 @@ class App extends Component {
       // projects: []
       projects: [
         {
-          project_name: "project 1",
+          project_name: 'project 1',
           palettes: [
             {
               palette_name: 'cool',
-              color_1: "#3D3935",
-              color_2: "#009DDC",
-              color_3: "#F26430",
-              color_4: "#F1F1F1",
-              color_5: "#009B72",
+              color_1: '#3D3935',
+              color_2: '#009DDC',
+              color_3: '#F26430',
+              color_4: '#F1F1F1',
+              color_5: '#009B72'
             },
             {
               palette_name: 'wowzah',
-              color_1: "#3D3935",
-              color_2: "#009DDC",
-              color_3: "#F26430",
-              color_4: "#F1F1F1",
-              color_5: "#009B72",
+              color_1: '#3D3935',
+              color_2: '#009DDC',
+              color_3: '#F26430',
+              color_4: '#F1F1F1',
+              color_5: '#009B72'
             }
           ]
         },
         {
-          project_name: "project TWO",
+          project_name: 'project TWO',
           palettes: [
             {
               palette_name: 'neat',
-              color_1: "#3D3935",
-              color_2: "#009DDC",
-              color_3: "#F26430",
-              color_4: "#F1F1F1",
-              color_5: "#009B72",
+              color_1: '#3D3935',
+              color_2: '#009DDC',
+              color_3: '#F26430',
+              color_4: '#F1F1F1',
+              color_5: '#009B72'
             }
           ]
-        },
+        }
       ]
     };
   }
 
   componentDidMount() {
-    this.randomizeHex()
+    this.randomizeHex();
   }
 
   randomizeHex = () => {
     const hexCodes = [];
     let i;
-    for( i = 0 ; i < 5 ; i++) {
-      hexCodes.push('#' + (Math.floor(Math.random() * 16777215).toString(16)));
+    for (i = 0; i < 5; i++) {
+      hexCodes.push('#' + Math.floor(Math.random() * 16777215).toString(16));
     }
-    this.setState({ hexCodes })
+    this.setState({ hexCodes });
   };
 
   render() {
     const projects = this.state.projects.map(project => {
-      return <ProjectContainer project = { project }/>
-    })
+      return <ProjectContainer project={project} />;
+    });
 
     return (
       <div className='App'>
-        <h1>Palette Picker</h1>
-        {/* <Nav /> */}
+        <Nav />
         {/* { projects } */}
-        <Container hexCodes = {this.state.hexCodes}/>
+        <Container hexCodes={this.state.hexCodes} />
       </div>
     );
   }
