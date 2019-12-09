dda0eeeeb6657cfc5d3a5e0d16fedd9731a96fee
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-08 21:14:43 -0700

    Implement routes in App file

--- src/Components/ProjectNav/ProjectNav.js
+++ src/Components/ProjectNav/ProjectNav.js
@@ -4,14 +4,14 @@ import './ProjectNav.scss';
 export class ProjectNav extends Component {
   render() {
     return (
-      <div className="ProjectNav">
-        <button className="ProjectNav__button--new-project">
-          New Project
-        </button>
-        <select className="ProjectNav__select--project">
+      <div className='ProjectNav'>
+        <button className='ProjectNav__button--new-project'>New Project</button>
+        <select className='ProjectNav__select--project'>
           {/* map through projects in state */}
         </select>
       </div>
-    )
+    );
   }
 }
+
+export default ProjectNav;
