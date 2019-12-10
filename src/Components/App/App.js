2bec8488fabec159a07f04dc8d3801e6c3c07441
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-10 14:20:56 -0700

    Update environmental variables to return a defined URL

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -60,7 +60,6 @@ class App extends Component {
 
   fetchProjects = async () => {
     let projects = await getAllProjects();
-    console.log('projects', projects)
     await this.setState({ projects });
   };
 
