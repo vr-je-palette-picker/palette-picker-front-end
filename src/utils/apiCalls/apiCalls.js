ac9cba81142b96ca5d5acae7e4eb5142092551c7
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-11 12:59:01 -0700

    Fix unhandled promise rejections in apiCalls.js

--- src/utils/apiCalls/apiCalls.js
+++ src/utils/apiCalls/apiCalls.js
@@ -72,6 +72,9 @@ export const createNewProject = async project => {
     `${process.env.REACT_APP_PRODUCTION_URL}/api/v1/projects`,
     options
   );
+  if (!response.ok) {
+    throw new Error('Could not create new project, please try again later.');
+  }
   const data = await response.json();
 
   return data;
@@ -89,6 +92,9 @@ export const createNewPalette = async palette => {
     `${process.env.REACT_APP_PRODUCTION_URL}/api/v1/palettes/${palette.project_id}`,
     options
   );
+  if (!response.ok) {
+    throw new Error('Could not create new palette, please try again later.');
+  }
   const data = await response.json();
 
   return data;
@@ -100,13 +106,19 @@ export const deleteProject = async id => {
     headers: {
       'Content-Type': 'application/json'
     }
+  };
+
+  const response = await fetch(
+    `${process.env.REACT_APP_PRODUCTION_URL}/api/v1/projects/${id}`,
+    options
+  );
+  if (!response.ok) {
+    throw new Error('Could not delete project, please try again later.');
   }
-  
-  const response = await fetch(`${process.env.REACT_APP_PRODUCTION_URL}/api/v1/projects/${id}`, options)
-  const data = response.json();
-  
+  const data = await response.json();
+
   return data;
-}
+};
 
 export const deletePalette = async id => {
   const options = {
@@ -114,10 +126,16 @@ export const deletePalette = async id => {
     headers: {
       'Content-Type': 'application/json'
     }
+  };
+
+  const response = await fetch(
+    `${process.env.REACT_APP_PRODUCTION_URL}/api/v1/palette/${id}`,
+    options
+  );
+  if (!response.ok) {
+    throw new Error('Could not delete palette, please try again later.');
   }
-  
-  const response = await fetch(`${process.env.REACT_APP_PRODUCTION_URL}/api/v1/palette/${id}`, options)
-  const data = response.json();
-  
+  const data = await response.json();
+
   return data;
-}
+};
