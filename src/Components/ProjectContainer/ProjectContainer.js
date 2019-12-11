5d98c58888fc7736ec927e82bc861d4eab26dc4a
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-12-11 12:23:35 -0700

    Render project name

--- src/Components/ProjectContainer/ProjectContainer.js
+++ src/Components/ProjectContainer/ProjectContainer.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PaletteCard from './../PaletteCard/PaletteCard.js';
 
 const ProjectContainer = ({ project }) => {
+  console.log(project.project_name)
   const paletteCards = project.palettes.map((palette, index) => {
     return <PaletteCard key={index} palette={palette} />;
   });
   return (
     <section className='ProjectContainer'>
       <h2 className='ProjectContainer__h2--project-name'>
-        {project.project_name}
+        {project.project}
       </h2>
       <section className='ProjectContainer__section'>{paletteCards}</section>
     </section>
