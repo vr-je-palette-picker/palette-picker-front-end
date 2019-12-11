08df0ae10f3ada8d618bf9098ac1d5f5891d3cfb
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-12-11 15:44:26 -0700

    Add functionality to delete palette on a click

--- src/Components/ProjectContainer/ProjectContainer.js
+++ src/Components/ProjectContainer/ProjectContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MdDeleteForever } from 'react-icons/md';
 import PaletteCard from './../PaletteCard/PaletteCard.js';
+import { deleteProject } from './../../utils/apiCalls/apiCalls.js'
 
 const ProjectContainer = ({ project }) => {
   const paletteCards = project.palettes.map((palette, index) => {
@@ -12,7 +13,10 @@ const ProjectContainer = ({ project }) => {
         <h2 className='ProjectContainer__h2--project-name'>
           {project.project}
         </h2>
-        <MdDeleteForever className='ProjectContainer__header--delete-icon'/>
+        <MdDeleteForever
+          className='ProjectContainer__header--delete-icon'
+          onClick={() => deleteProject(project.id)}
+        />
       </header>
       <section className='ProjectContainer__section'>{paletteCards}</section>
     </section>
