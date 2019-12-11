7bfe804669ce2fb5e9d4ff0fc808db84cfb7718e
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-10 22:11:23 -0700

    Update snapshot test for PaletteForm Component with modal and post method props

--- src/Components/PaletteForm/PaletteForm.test.js
+++ src/Components/PaletteForm/PaletteForm.test.js
@@ -4,12 +4,23 @@ import PaletteForm from './PaletteForm';
 
 describe('PaletteForm', () => {
   let wrapper;
+  let projects = [{}, {}, {}];
+  let close = jest.fn();
+  let newPalette = jest.fn();
+  let findProjectByName = jest.fn();
 
   beforeEach(() => {
-    wrapper = shallow(<PaletteForm />)
-  })
+    wrapper = shallow(
+      <PaletteForm
+        projects={projects}
+        close={close}
+        newPalette={newPalette}
+        findProjectByName={findProjectByName}
+      />
+    );
+  });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
-  })
-})
+  });
+});
